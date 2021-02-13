88052ee77f556d0fef08a52be3f577fa1bf1a5ba
Dimitar Grudev <dimitar.grudev@bianor.com>
2021-02-13 14:27:44 +0200

    initial commit

---
+++ LionheartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LionheartApp
+//
+//  Created by Dimitar Grudev on 13.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

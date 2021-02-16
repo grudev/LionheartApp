66c6d93188c8f3249d5356cd4808c2e4a94a76ee
Dimitar Grudev <dk.grudev@gmail.com>
2021-02-16 20:50:18 +0200

    Add Network Manager and GalleryRepository

---
+++ LionheartApp/Network/Repositories/GalleryRepository.swift
@@ -0,0 +1,37 @@
+//
+//  GalleryRepository.swift
+//  LionheartApp
+//
+//  Created by Dimitar Grudev on 16.02.21.
+//
+
+import Foundation
+
+typealias GalleryDataType = ResponseData<Gallery>
+typealias GalleryResultType = (Result<GalleryDataType, Error>) -> Void
+
+protocol GalleryRepository {
+    init(_ networkManager: NetworkManager)
+    func getGallery(_ completion: @escaping GalleryResultType) -> NetworkCancellable?
+}
+
+final class NetworkGalleryRepository: GalleryRepository {
+    
+    private var networkManager: NetworkManager!
+    
+    init(_ networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func getGallery(_ completion: @escaping GalleryResultType) -> NetworkCancellable? {
+        do {
+            let request = try APIRouter.dogsGallery.asURLRequest()
+            let cancelable = networkManager.request(request, completion: completion)
+            return cancelable
+        } catch {
+            completion(.failure(error))
+            return nil
+        }
+    }
+    
+}
